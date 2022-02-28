888c3684534e1864665e2bc5152921b37797a73c
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-02-28 23:46:38 +0630

    GET launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -10,6 +10,12 @@ async function httpGetPlanets() {
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
+   // Load launches and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches =  await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
   // Load launches, sort by flight number, and return as JSON.
 }
 
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,16 @@
+const launches = new Map();
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-442 b',
+    customer: ['BRNYR','NASA'],
+    upcoming: true,
+    success:true,
+}
+
+launches.set(launch.flightNumber,launch);
+
+module.exports = { launches}
