d048066457b96a3f51fb11da5aee134a35d8a01b
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-03-01 00:42:33 +0630

    POST launches Validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,7 +10,18 @@ function httpgetAllLaunches(req, res){
 function httpAddNewLaunch(req,res){
     const launch = req.body;
 
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+        return res.status(404).json({
+            error: 'Missing required launch property'
+        });
+    }
+
     launch.launchDate = new Date( launch.launchDate);
+    if(isNaN(launch.launchDate)){
+        return res.status(404).json({
+            error: 'Invalide launch date'
+        })
+    }
 
     addNewLaunch(launch);
     res.status(201).json(launch);
