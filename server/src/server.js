c27cc89f3fc78958fd9a1df58df274225b713f10
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-03-03 20:47:57 +0630

    connecting mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,25 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+// Update below to match your own MongoDB connection string.
+
+const MONGO_URL = 'mongodb+srv://kzh:kaungzawhein1963@nasacluster.gsndy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('error tat naydl')
+});
+
 async function startServer() {
+
+    await mongoose.connect(MONGO_URL);
+   
     await loadPlanetsData();
   
     server.listen(PORT, () => {
